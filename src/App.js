7c6ce3cf1281fe8a7158c6d7af63117196b47fd4
mss38 <email@example.com>
2022-04-03 23:05:44 +0600

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Blogs from "./components/Blogs/Blogs";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="home" element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-   return (
-      <div>
-         <h2>This is Header</h2>
-      </div>
-   );
+  return (
+    <div>
+      <nav className="uppercase space-x-4 text-center  mt-5 mb-4 font-bold font-mono text-xl text-fuchsia-900">
+        <Link to="/home">Home</Link>
+        <Link to="/reviews">Reviews</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/blogs">Blogs</Link>
+        <Link to="/about">About</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
